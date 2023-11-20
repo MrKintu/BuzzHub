4823089ef0028bd655d40ef4b74c018eecb24bad
Kintu Declan <d-kintu@outlook.com>
2023-11-20 15:00:16 -0500

    Try this

--- backend/authy/models.py
+++ backend/authy/models.py
@@ -33,7 +33,7 @@ class Profile(models.Model):
                              on_delete=models.CASCADE)
     image = models.ImageField(upload_to="profile_picture", null=True,
                               default="default.jpg")
-    id_document = models.ImageField(storage=rename_id, null=True,
+    id_document = models.ImageField(upload_to=rename_id, null=True,
                                     blank=True)
     doc_id = models.CharField(max_length=200, null=True, blank=True)
     d_o_b = models.DateTimeField(auto_now=False, auto_now_add=False, null=True,
