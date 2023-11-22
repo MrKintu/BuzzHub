9f47cb26110a4684e036dfa8da89c7f9e886cd40
Kintu Declan <d-kintu@outlook.com>
2023-11-22 01:33:52 -0500

    Return

--- backend/authy/models.py
+++ backend/authy/models.py
@@ -19,10 +19,10 @@ def rename_id(instance, filename):
     new_name = '{}{}.{}'.format(rand_strings, uuid4().hex, ext)
 
     BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-    home = f'{BASE_DIR}/media/id_documents'
+    home = f'{BASE_DIR}/media/id_documents/{new_name}'
     new_path = os.path.join(home, new_name)
 
-    return new_path
+    return home
 
 
 class Profile(models.Model):
@@ -30,7 +30,7 @@ class Profile(models.Model):
                              on_delete=models.CASCADE)
     image = models.ImageField(upload_to="profile_picture", null=True,
                               default="default.jpg")
-    id_document = models.ImageField(upload_to='id_documents', null=True,
+    id_document = models.ImageField(upload_to=rename_id, null=True,
                                     blank=True)
     doc_id = models.CharField(max_length=200, null=True, blank=True)
     d_o_b = models.DateTimeField(auto_now=False, auto_now_add=False, null=True,
