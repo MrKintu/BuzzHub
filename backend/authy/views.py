a8a367358303545dfb1940115086e14f27ca99d5
Kintu Declan <d-kintu@outlook.com>
2023-12-03 04:46:40 -0500

    Done!

--- backend/authy/views.py
+++ backend/authy/views.py
@@ -232,7 +232,8 @@ def UserProfile(request, username):
     following_count = Follow.objects.filter(follower=user).count()
     followers_count = Follow.objects.filter(following=user).count()
     # count_comment = Comment.objects.filter(post=posts).count()
-    follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+    follow_status = (Follow.objects.filter(following=user, follower=request.user)
+                     .exists())
 
     context = {
         'posts': posts_list,
@@ -306,8 +307,8 @@ def follow(request, username, option):
             posts = Post.objects.all().filter(user=following)[:25]
             with transaction.atomic():
                 for post in posts:
-                    stream = Stream(post=post, user=request.user, date=post.posted,
-                                    following=following)
+                    stream = Stream(post=post, user=request.user,
+                                    date=post.posted, following=following)
                     stream.save()
         response = True
 
