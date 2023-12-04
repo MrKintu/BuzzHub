96c1951d310c16f3ca6a8c12629fded30a0776d8
Kintu Declan <d-kintu@outlook.com>
2023-12-03 19:12:12 -0500

    Done!

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -3,5 +3,5 @@ from notification.views import ShowNotification, DeleteNotification
 
 urlpatterns = [
     path('', ShowNotification, name='show-notification'),
-    path('<noti_id>/delete', DeleteNotification, name='delete-notification'),
+    path('<noti_id>/delete/', DeleteNotification, name='delete-notification'),
 ]
--- backend/post/urls.py
+++ backend/post/urls.py
@@ -3,10 +3,10 @@ from post.views import index, NewPost, PostDetail, Tags, like, favourite
 
 urlpatterns = [
     path('', index, name='index'),
-    path('new-post', NewPost, name='new-post'),
-    path('<uuid:post_id>', PostDetail, name='post-details'),
-    path('tag/<slug:tag_slug>', Tags, name='tags'),
-    path('<uuid:post_id>/like', like, name='like'),
-    path('<uuid:post_id>/favourite', favourite, name='favourite'),
+    path('new-post/', NewPost, name='new-post'),
+    path('<uuid:post_id>/', PostDetail, name='post-details'),
+    path('tag/<slug:tag_slug>/', Tags, name='tags'),
+    path('<uuid:post_id>/like/', like, name='like'),
+    path('<uuid:post_id>/favourite/', favourite, name='favourite'),
 
 ]
