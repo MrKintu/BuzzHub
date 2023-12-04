9d8319d3a922d47a56d3441bb912f8bf2dd8d329
Kintu Declan <d-kintu@outlook.com>
2023-12-03 19:12:59 -0500

    Done!

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/', Tags, name='tags'),
     path('<uuid:post_id>/like/', like, name='like'),
     path('<uuid:post_id>/favourite/', favourite, name='favourite'),
-
 ]
